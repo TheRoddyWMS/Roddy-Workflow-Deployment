99e56c8d1e218f4771afc25f2bcaf297ef3d667d
Michael Heinold <dankwart-de@users.noreply.github.com>
2017-09-11 11:07:51 +0200

    Update roddy.sh
    Add tumor and control sample names and add them as variables to the roddy call.

--- docker/ACEseqDockerContainer/roddy.sh
+++ docker/ACEseqDockerContainer/roddy.sh
@@ -4,12 +4,14 @@
 # 3: Dataset identifiert / PID
 # 4: The control bam file
 # 5: The tumor bam file
-# 6: The reference genome file path
-# 7: The reference files path
-# 8: The output folder
-# 9: Optional: The SV file
+# 6: The control bam sample name
+# 7: The tumor bam sample name
+# 8: The reference genome file path
+# 9: The reference files path
+# 10: The output folder
+# 11: Optional: The SV file
 
-[[ $# -lt 8 ]] && echo "Wrong number of arguments" && (head -n 10 $0 | tail -n+2) && exit 1
+[[ $# -lt 10 ]] && echo "Wrong number of arguments" && (head -n 10 $0 | tail -n+2) && exit 1
 
 ## Read in parameters and check files and folders
 mode=${1}
@@ -18,9 +20,11 @@ pid=${3}
 
 inputBamCtrlLcl=`readlink -f ${4}`
 inputBamTumorLcl=`readlink -f ${5}`
-referenceGenomePath=`dirname ${6}`
-referenceFilePath=`dirname ${7}`
-workspaceLcl=`readlink -f ${8}`
+inputBamCtrlSampleName=${6}
+inputBamTumorSampleName=${7}
+referenceGenomePath=`dirname ${8}`
+referenceFilePath=`dirname ${9}`
+workspaceLcl=`readlink -f ${10}`
 
 function checkFile() {
 	local _file=${1}
@@ -45,9 +49,9 @@ checkDir $referenceGenomePath
 checkDir $referenceFilePath 
 checkDir $workspaceLcl rw
 
-if [[ $# -eq 9 ]]; then
+if [[ $# -eq 11 ]]; then
 	# Either use the file
-	local svFile=`readlink -f ${9}`
+	local svFile=`readlink -f ${11}`
 	checkFile $svFile
 	svBlock="svFile:${svFile}"
 else 
@@ -69,9 +73,11 @@ referenceFilesFolder=
 roddyBinary="bash /home/roddy/binaries/Roddy/roddy.sh"
 roddyConfig="--useconfig=/home/roddy/config/config.ini"
 bamFiles="bamfile_list:$inputBamCtrl;$inputBamTumor"
+sampleList="sample_list:${inputBamCtrlSampleName};${inputBamTumorSampleName}"
+tumorSample="tumorSample:${inputBamTumorSampleName}
 
 # Kortine, do you need to set this? ,REFERENCE_GENOME:${referenceGenome}
-call="${roddyBinary} ${mode} ${configurationIdentifier}@copyNumberEstimation ${pid} ${roddyConfig} --cvalues=\"${bamFiles},${svBlock}\""
+call="${roddyBinary} ${mode} ${configurationIdentifier}@copyNumberEstimation ${pid} ${roddyConfig} --cvalues=\"${bamFiles},${svBlock},${sampleList},${tumorSample}\""
 
 # Change group and user id to match your system user and group
 sed "s/:1000:1000:/:`id -u`:`id -g`:/" passwdfile > passwdtemp
